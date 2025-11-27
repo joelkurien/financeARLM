65c7c0445cb1469f721194419a5a64ba4a052ff6
Joelkurien <joelkurien7@gmail.com>
2025-11-27 22:52:06 +1100

    Added memory views for in place 2d operations and slicing

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,30 @@ using namespace std;
 using namespace Eigen;
 
 int main(){
-    Tensor tensor(2,2);
+    Tensor tensor(3,3);
     auto [row, col] = tensor.shape();
-    tensor.put(1,1,9);
-    cout<<tensor[1][1]<<endl;
-    cout<<tensor.empty()<<endl;
-    cout<<tensor.size()<<endl;
+    int s = 0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            tensor.put(i,j,s++);
+        }
+    }
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout<<tensor[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    auto [smaddr, height, width] = tensor.view(1, 2, 1, 2);
+    Tensor slc = tensor.slice(1,1,2,2);
+    auto [sr, sc] = slc.shape();
+    for(int i=0; i<sr; i++){
+        for(int j=0; j<sc; j++){
+            cout<<slc[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     return 0;
 }
--- src/tensor.h
+++ src/tensor.h
@@ -11,7 +11,8 @@ using namespace Eigen;
 
 class Tensor {
     private:
-        MatrixXd matrix;
+        Matrix<double, Dynamic, Dynamic, RowMajor> matrix;
+        vector<Matrix<double, Dynamic, Dynamic, RowMajor>> batches;
         size_t mrows;
         size_t mcols;
 
@@ -72,6 +73,28 @@ class Tensor {
             return (row == 0 && col == 0);
         }
         
-        
+        tuple<double*, size_t, size_t> view(int rstart, int rend, int cstart, int cend){
+            double* basePtr = matrix.data();
+            double* subBasePtr = basePtr + (mcols*rstart + cstart);
+            double height = rend-rstart+1;
+            double width = cend-cstart+1;
+            return {subBasePtr, height, width};
+        }
+
+        Tensor slice(int rstart, int cstart, int rend, int cend){
+            auto[smaddr, height, width] = view(rstart, rend, cstart, cend);
+            if(rstart < 0 || rstart > mrows || rend < 0 || rend > mrows )
+                throw out_of_range("Row Index out of bounds");
+            if(cstart < 0 || cstart > mcols || cend < 0 || cend > mcols)
+                throw out_of_range("Col Index out of bounds");
+            
+            Tensor slc(height, width);
+            for(int i=0; i<height; i++){
+                for(int j=0; j<width; j++){
+                    slc.put(i,j,*(smaddr + mcols*i + j));
+                }
+            }
+            return slc;
+        }
 };
 #endif
