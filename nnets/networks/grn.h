a8ca5a0952eaae02dda3911bc22c3ed923a92425
Joelkurien <joelkurien7@gmail.com>
2026-01-26 02:52:56 +1100

    Expose layer parameters in GRN for training

--- nnets/networks/grn.h
+++ nnets/networks/grn.h
@@ -4,23 +4,45 @@
 #include "autograd.h"
 #include "Linear.h"
 #include "activations.h"
-#include "RootLayer.h"
 #include "normalizations.h"
 #include "dropout.h"
+#include "RootLayer.h"
 
-class GRN {
-    RootLayer* layer;
+/*
+ * Gated Residual Network
+ * */
+class GRN : RootLayer {
     public:
-        GRN() = default;
+        GRN(): RootLayer() {};
 
-        std::shared_ptr<TensorX> forward(std::shared_ptr<TensorX> input) {
-            layer->set(Linear(512).forward(input));
-            layer->set(ELU(1).forward(layer->data()));
-            layer->set(Linear(256).forward(layer->data()));
-            layer->set(Dropout(0.3).forward(layer->data()));
-            layer->set(add(input, GLU().forward(layer->data())));
-            layer->set(LayerNormalization().forward(layer->data()));
-            return layer->data();
+        Linear linear_layer_1{512, true};
+        ELU elu_layer{1};
+        Linear linear_layer_2{256, true};
+        Dropout dropout_layer{0.3};
+        GLU glu_layer;
+        LayerNormalization layer_norm;
+
+        virtual std::shared_ptr<TensorX> forward(std::shared_ptr<TensorX> input) override {
+            std::shared_ptr<TensorX> x = linear_layer_1.forward(input);
+            x = elu_layer.forward(x);
+            x = linear_layer_2.forward(x);
+            x = dropout_layer.forward(x);
+
+            std::shared_ptr<TensorX> residual = add(input, glu_layer.forward(x));
+            return layer_norm.forward(residual);
+        }
+
+        virtual std::vector<std::shared_ptr<TensorX>> parameters() override {
+            std::vector<std::shared_ptr<TensorX>> params;
+            std::vector<std::shared_ptr<TensorX>> l1_params = linear_layer_1.parameters();
+            std::vector<std::shared_ptr<TensorX>> l2_params = linear_layer_2.parameters();
+            std::vector<std::shared_ptr<TensorX>> norm_params = layer_norm.parameters();
+            
+            params.insert(params.end(), l1_params.begin(), l1_params.end());
+            params.insert(params.end(), l2_params.begin(), l2_params.end());
+            params.insert(params.end(), norm_params.begin(), norm_params.end());
+
+            return params;
         }
 };
 #endif
