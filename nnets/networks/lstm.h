6ba41dc8f4689eae4750219040725fc6b6b4a324
Joelkurien <joelkurien7@gmail.com>
2026-02-01 23:58:06 +1100

    Created the LSTM Seq2Seq Layer

---
+++ nnets/networks/lstm.h
@@ -0,0 +1,77 @@
+#include "autograd.h"
+#include "RootLayer.h"
+#include "Linear.h"
+#include "activations.h"
+
+struct LSTM_Contexts {
+    std::shared_ptr<TensorX> hidden_t;
+    std::shared_ptr<TensorX> cell_t;
+};
+
+class LSTM : public RootLayer {
+    size_t in_features;
+    size_t out_features;
+    bool initialize;
+
+    std::vector<Linear> linear_layers;
+    
+    public:
+        LSTM(size_t in_feat, size_t out_feat, bool init)
+            : in_features(in_feat), out_features(out_feat), initialize(init)
+        {
+            for(int i=0; i<4; i++){
+                linear_layers.push_back(Linear(in_features, out_features, initialize));
+                linear_layers.push_back(Linear(out_features, out_features, initialize));
+            }
+
+        }
+        
+        std::vector<LSTM_Contexts> forward(std::shared_ptr<TensorX> input, 
+                                            std::shared_ptr<TensorX> h_0, 
+                                            std::shared_ptr<TensorX> cell_0,
+                                            size_t num_splits){
+            
+            std::shared_ptr<TensorX> forget_x = linear_layers[0].forward(input);
+            std::shared_ptr<TensorX> ignore_x = linear_layers[2].forward(input);
+            std::shared_ptr<TensorX> candidate_x = linear_layers[4].forward(input);
+            std::shared_ptr<TensorX> filter_x = linear_layers[6].forward(input);
+
+            std::vector<std::shared_ptr<TensorX>> forget_x_list = chunk(forget_x, num_splits, 0);
+            std::vector<std::shared_ptr<TensorX>> ignore_x_list = chunk(ignore_x, num_splits, 0);
+            std::vector<std::shared_ptr<TensorX>> candidate_x_list = chunk(candidate_x, num_splits, 0);
+            std::vector<std::shared_ptr<TensorX>> filter_x_list = chunk(filter_x, num_splits, 0);
+            
+            std::vector<LSTM_Contexts> lstm_contexts_list = {{h_0, cell_0}};
+            std::shared_ptr<TensorX> h_prev = h_0;
+            std::shared_ptr<TensorX> cell_prev = cell_0;
+            
+            for(size_t idx=0; idx<num_splits; idx++){
+                std::shared_ptr<TensorX> f = Sigmoid().forward(add(forget_x_list[idx], linear_layers[1].forward(h_prev)));
+                std::shared_ptr<TensorX> i = Sigmoid().forward(add(ignore_x_list[idx], linear_layers[3].forward(h_prev)));
+                std::shared_ptr<TensorX> c = Tanh().forward(add(ignore_x_list[idx], linear_layers[5].forward(h_prev)));
+                std::shared_ptr<TensorX> o = Sigmoid().forward(add(ignore_x_list[idx], linear_layers[7].forward(h_prev)));
+
+                cell_prev = add(multiply(f, cell_prev), multiply(i, c));
+                h_prev = multiply(o, tanh(cell_prev));
+
+                lstm_contexts_list.push_back({h_prev, cell_prev});
+            }
+
+            return lstm_contexts_list; 
+        }
+
+        virtual std::vector<std::shared_ptr<TensorX>> parameters() override {
+            std::vector<std::shared_ptr<TensorX>> params;
+            
+            auto append_params = [&](RootLayer& layer){
+                for(std::shared_ptr<TensorX>& p: layer.parameters()){
+                    params.push_back(p);
+                }
+            };
+            
+            for(int i=0; i<8; i++){
+                append_params(linear_layers[i]);
+            } 
+            return params;
+        }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "static_encoder.h"
 
 int main(){
-    Tensor data = arange({10,5}) / 10000;
+    Tensor data = arange({2,10,5}) / 100;
     std::shared_ptr<TensorX> data_tensor = tensor::create(data);
 
     Static_Encoder se(256, 0.3, 5, 5);
@@ -17,19 +17,19 @@ int main(){
     Static_Contexts se_res = se.forward_all(data_tensor);
     // for(size_t i=0; i<2; i++){
     //     for(size_t j=0; j<10; j++){
-    //         for(size_t k=0; k<1; k++){
+    //         for(size_t k=0; k<256; k++){
     //             std::cout<<vsn_res->get_data().at({i,j,k})<<" ";
     //         }
     //         std::cout<<std::endl;
     //     }
     //     std::cout<<std::endl;
     // }
-    for(size_t i=0; i<10; i++){
-        for(size_t j=0; j<256; j++){
-            std::cout<<se_res.cs->get_data().at({i,j})<<" ";
-        }
-        std::cout<<std::endl;
-    }
+    // for(size_t i=0; i<10; i++){
+    //     for(size_t j=0; j<256; j++){
+    //         std::cout<<se_res.cs->get_data().at({i,j})<<" ";
+    //     }
+    //     std::cout<<std::endl;
+    // }
 
     prnt(vsn_res->get_data().shape());
 
